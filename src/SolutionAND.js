6976ba67eb3bd1f38ab2c6d435f2d22511509440
joannacollier <collj035@gmail.com>
2020-10-15 19:41:13 +0100

    removed console and comments

--- src/SolutionAND.js
+++ src/SolutionAND.js
@@ -1,28 +1,27 @@
-function solution (input) { // 123
+function solution (input) { 
 
   if(input.length < 2){
     return input
   } 
 
-  let numberArray = input.split('') // [1,2,3]
-  let results = [[numberArray.shift()]] // 1 
+  let numberArray = input.split('') 
+  let results = [[numberArray.shift()]] 
   while(numberArray.length){
     
-    const currentNumber = numberArray.shift() // 2
+    const currentNumber = numberArray.shift() 
     let tempResults = [] 
 
-    results.forEach(result => { // [1]
-      console.log(result)
+    results.forEach(result => { 
       let resultIndex = 0
       
-      while(resultIndex <= result.length){ // 1 
-        const tempContainer = [...result] /// [1]
-        tempContainer.splice(resultIndex, 0, currentNumber) /// tempcontainer = [2,1]
+      while(resultIndex <= result.length){  
+        const tempContainer = [...result] 
+        tempContainer.splice(resultIndex, 0, currentNumber) 
         tempResults.push(tempContainer)
         resultIndex ++ 
       }
     })
-    results = tempResults // [2, 1]
+    results = tempResults 
   }
   return results
     .map(numbers => numbers.join(''))
@@ -30,6 +29,5 @@ function solution (input) { // 123
     .join()
 }
 
-// some example inputs
-// console.log(solution('326')); // expected ouput 632,623,362,326,263,236
-// console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
+console.log(solution('326')); // expected ouput 632,623,362,326,263,236
+console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
