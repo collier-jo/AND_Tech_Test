fd84a16a105a1bd829f6873cda8e30ed68bd9242
joannacollier <collj035@gmail.com>
2020-10-15 19:39:48 +0100

    Double loop to iterate over each digit

--- spec/SolutionSpec.js
+++ spec/SolutionSpec.js
@@ -3,4 +3,8 @@ describe("Solution", function() {
   it("Single Digit Solution", function() {
     expect(solution("1")).toEqual("1")
   });
+
+  it("Double Digit Solution", function() {
+    expect(solution("12")).toEqual("21,12")
+  });
 });
--- src/SolutionAND.js
+++ src/SolutionAND.js
@@ -1,16 +1,35 @@
-/**
-* The following is the function where the solution shall be written
-*/
-
-function solution (input) {
+function solution (input) { // 123
 
   if(input.length < 2){
     return input
-  }
+  } 
 
-  return null; 
+  let numberArray = input.split('') // [1,2,3]
+  let results = [[numberArray.shift()]] // 1 
+  while(numberArray.length){
+    
+    const currentNumber = numberArray.shift() // 2
+    let tempResults = [] 
+
+    results.forEach(result => { // [1]
+      console.log(result)
+      let resultIndex = 0
+      
+      while(resultIndex <= result.length){ // 1 
+        const tempContainer = [...result] /// [1]
+        tempContainer.splice(resultIndex, 0, currentNumber) /// tempcontainer = [2,1]
+        tempResults.push(tempContainer)
+        resultIndex ++ 
+      }
+    })
+    results = tempResults // [2, 1]
+  }
+  return results
+    .map(numbers => numbers.join(''))
+    .sort(function(a, b){return b-a})
+    .join()
 }
 
 // some example inputs
-console.log(solution('326')); // expected ouput 632,623,362,326,263,236
-console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
+// console.log(solution('326')); // expected ouput 632,623,362,326,263,236
+// console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
